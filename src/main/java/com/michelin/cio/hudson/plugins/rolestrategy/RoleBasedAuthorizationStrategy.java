6c2e145e30cac3ac5e060f0e97a719b9d264a946
Oleg Nenashev <nenashev@synopsys.com>
2013-12-07 17:57:51 +0400

    [JENKINS-18978] - Fixed a groups list for slaves. Resolves: https://issues.jenkins-ci.org/browse/JENKINS-18978
    Signed-off-by: Oleg Nenashev <nenashev@synopsys.com>

--- src/main/java/com/michelin/cio/hudson/plugins/rolestrategy/RoleBasedAuthorizationStrategy.java
+++ src/main/java/com/michelin/cio/hudson/plugins/rolestrategy/RoleBasedAuthorizationStrategy.java
@@ -41,7 +41,10 @@ import hudson.model.Computer;
 import hudson.model.Hudson;
 import hudson.model.Item;
 import hudson.model.Job;
+import hudson.model.Project;
+import hudson.model.Run;
 import hudson.model.View;
+import hudson.scm.SCM;
 import hudson.security.ACL;
 import hudson.security.AccessControlled;
 import hudson.security.AuthorizationStrategy;
@@ -574,7 +577,11 @@ public class RoleBasedAuthorizationStrategy extends AuthorizationStrategy {
             groups.remove(PermissionGroup.get(Permission.class));
             groups.remove(PermissionGroup.get(Hudson.class));
             groups.remove(PermissionGroup.get(View.class));
-            groups.remove(PermissionGroup.get(Job.class));
+            
+            // Project, SCM and Run permissions 
+            groups.remove(PermissionGroup.get(Item.class));
+            groups.remove(PermissionGroup.get(SCM.class));
+            groups.remove(PermissionGroup.get(Run.class));
         }
         else {
             groups = null;
