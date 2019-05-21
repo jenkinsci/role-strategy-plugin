82dc3309bfb85e507ec5ac5049523edc63c7c475
Abhyudaya Sharma <sharmaabhyudaya@gmail.com>
2019-05-21 11:06:45 +0530

    Use JUnit Assert for checking if Jenkins instance is null

--- src/test/java/jmh/SampleBenchmark.java
+++ src/test/java/jmh/SampleBenchmark.java
@@ -6,18 +6,18 @@ import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.infra.Blackhole;
 
+import static org.junit.Assert.assertNotNull;
+
 public class SampleBenchmark {
     @State(Scope.Benchmark)
     public static class JenkinsState extends JmhBenchmarkState {
     }
 
     @Benchmark
-    public void testMethod(JenkinsState state, Blackhole blackhole) throws Exception {
+    @SuppressWarnings("unused")
+    public void testMethod(JenkinsState state, Blackhole blackhole) {
         Jenkins jenkins = Jenkins.getInstanceOrNull();
-        if (jenkins == null) {
-            throw new Exception("Unable to start Jenkins");
-        } else {
-            blackhole.consume(jenkins);
-        }
+        assertNotNull("Unable to find Jenkins Instance", jenkins);
+        blackhole.consume(jenkins);
     }
 }
