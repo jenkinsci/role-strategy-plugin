1fbd3f005e61c2621eb99761c5d97f4d74a5035e
Hui Jun Ng <hui.jun.ng@autodesk.com>
2020-09-30 23:41:28 +0800

    modify two tests to use webrequest

--- src/test/java/com/michelin/cio/hudson/plugins/rolestrategy/APITest.java
+++ src/test/java/com/michelin/cio/hudson/plugins/rolestrategy/APITest.java
@@ -1,6 +1,13 @@
 package com.michelin.cio.hudson.plugins.rolestrategy;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gargoylesoftware.htmlunit.HttpMethod;
+import com.gargoylesoftware.htmlunit.Page;
+import com.gargoylesoftware.htmlunit.WebRequest;
+import com.gargoylesoftware.htmlunit.util.NameValuePair;
 import com.synopsys.arc.jenkins.plugins.rolestrategy.RoleType;
+import hidden.jth.org.apache.http.HttpRequest;
+import hidden.jth.org.apache.http.auth.UsernamePasswordCredentials;
 import hudson.model.User;
 import org.acegisecurity.context.SecurityContext;
 import org.acegisecurity.context.SecurityContextHolder;
@@ -11,6 +18,12 @@ import static org.junit.Assert.*;
 import org.jvnet.hudson.test.JenkinsRule;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.SortedMap;
@@ -24,9 +37,10 @@ public class APITest {
     @Rule
     public final JenkinsRule jenkinsRule = new JenkinsRule();
     public static Logger LOGGER = Logger.getLogger(APITest.class.getName());
+    JenkinsRule.WebClient webClient;
 
     @Before
-    public void setUp() throws IOException {
+    public void setUp() throws Exception {
         // Setting up jenkins configurations
         jenkinsRule.jenkins.setSecurityRealm(jenkinsRule.createDummySecurityRealm());
         jenkinsRule.jenkins.setAuthorizationStrategy(new RoleBasedAuthorizationStrategy());
@@ -38,19 +52,27 @@ public class APITest {
         RoleBasedAuthorizationStrategy.getInstance().doAssignRole("globalRoles", "adminRole", "adminUser");
         SecurityContext securityContext = SecurityContextHolder.getContext();
         securityContext.setAuthentication(User.getById("adminUser", true).impersonate());
+        webClient = jenkinsRule.createWebClient();
+        webClient.login("adminUser", "adminUser");
     }
 
     @Test
     public void testAddRole() throws IOException {
-        // Adding role via curl command
         String roleName = "new-role";
         String pattern = "test-folder.*";
-        String url = jenkinsRule.jenkins.getRootUrl() + "role-strategy/strategy/addRole";
-        String curlCmd = "curl -u adminUser:adminUser -X POST " + url + " --data type="+ RoleType.Project.getStringType()
-                + "&roleName=" + roleName + "&permissionIds=hudson.model.Item.Configure&overwrite=false"
-                + "&pattern=" + pattern;
-        String output = execCmd(curlCmd);
-        LOGGER.info("Output of addRole: " + output);
+        // Adding role via web request
+        URL apiURL = new URL(jenkinsRule.jenkins.getRootUrl() + "role-strategy/strategy/addRole");
+        WebRequest request = new WebRequest(apiURL, HttpMethod.POST);
+        request.setRequestParameters(Arrays.asList(
+                new NameValuePair("type", RoleType.Project.getStringType()),
+                new NameValuePair("roleName", roleName),
+                new NameValuePair("permissionIds", "hudson.model.Item.Configure"),
+                new NameValuePair("overwrite", "false"),
+                new NameValuePair("pattern", pattern)
+        ));
+        Page page = webClient.getPage(request);
+        assertEquals("Testing if post request is successful", HttpURLConnection.HTTP_OK, page.getWebResponse().getStatusCode());
+
         // Verifying that the role is in
         RoleBasedAuthorizationStrategy strategy = RoleBasedAuthorizationStrategy.getInstance();
         SortedMap<Role, Set<String>> grantedRoles = strategy.getGrantedRoles(RoleType.Project);
@@ -66,13 +88,17 @@ public class APITest {
 
     @Test
     public void testGetRole() throws IOException {
-        String curlCmd = "curl -u adminUser:adminUser -X GET " + jenkinsRule.jenkins.getRootUrl()
-                + "role-strategy/strategy/getRole?" + "type=" + RoleType.Global.getStringType() + "&roleName=adminRole";
-        String roleString = execCmd(curlCmd);
-        LOGGER.info("response: " + roleString);
+        String url = jenkinsRule.jenkins.getRootUrl() + "role-strategy/strategy/getRole?type="
+                + RoleType.Global.getStringType() + "&roleName=adminRole";
+        URL apiURL = new URL(url);
+        WebRequest request = new WebRequest(apiURL, HttpMethod.POST);
+        Page page = webClient.getPage(request);
 
+        // Verifying that web request is successful and that the role is found
+        assertEquals("Testing if post request is successful", HttpURLConnection.HTTP_OK, page.getWebResponse().getStatusCode());
+        String roleString = page.getWebResponse().getContentAsString();
         assertTrue(roleString.length() > 2);
-        assertNotEquals("{}", roleString);
+        assertNotEquals("{}", roleString);  // {} is returned when no role is found
     }
 
     @Test
