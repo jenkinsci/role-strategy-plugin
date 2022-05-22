60bc60e3bc9a3d2b9010fa4c1b812b61122c4bee
Markus Winter <m.winter@sap.com>
2022-05-23 00:49:56 +0200

    use iconset and icon classes
    avoids the differentiation of jenkins version

--- src/main/java/com/michelin/cio/hudson/plugins/rolestrategy/ValidationUtil.java
+++ src/main/java/com/michelin/cio/hudson/plugins/rolestrategy/ValidationUtil.java
@@ -1,17 +1,19 @@
 package com.michelin.cio.hudson.plugins.rolestrategy;
 
+import org.jenkins.ui.icon.Icon;
+import org.jenkins.ui.icon.IconSet;
+import org.kohsuke.accmod.Restricted;
+import org.kohsuke.accmod.restrictions.NoExternalUse;
 import org.kohsuke.stapler.Stapler;
 
-import hudson.util.VersionNumber;
 import jenkins.model.Jenkins;
 
+@Restricted(NoExternalUse.class)
 class ValidationUtil {
   private ValidationUtil() {
       // do not use
   }
 
-  private static final VersionNumber jenkinsVersion = Jenkins.getVersion();
-  
   static String formatNonExistentUserGroupValidationResponse(String user, String tooltip) {
       return formatUserGroupValidationResponse(null, "<span style='text-decoration: line-through; color: grey;'>" + user + "</span>", tooltip);
   }
@@ -21,10 +23,9 @@ class ValidationUtil {
           return String.format("<span title='%s'>%s</span>", tooltip, user);
       }
 
-      String imageFormat = String.format("svgs/%s.svg' width='16'", img);
-      if (jenkinsVersion.isOlderThan(new VersionNumber("2.308"))) {
-        imageFormat = String.format("16x16/%s.png'", img);
-      }
-      return String.format("<span title='%s'><img src='%s%s/images/%s style='margin-right:0.2em'>%s</span>", tooltip, Stapler.getCurrentRequest().getContextPath(), Jenkins.RESOURCE_PATH, imageFormat, user);
+      String imageFormat = String.format("icon-%s icon-sm", img);
+      Icon icon = IconSet.icons.getIconByClassSpec(imageFormat);
+      return String.format("<span title='%s'><img src='%s%s/images/%s' style='%s margin-right:0.2em'>%s</span>",
+            tooltip, Stapler.getCurrentRequest().getContextPath(), Jenkins.RESOURCE_PATH, icon.getUrl(), icon.getStyle(), user);
   }
 }
