fda31e25c7385d0dfb4f1eb9c95042a9c8e04be9
Markus Winter <m.winter@sap.com>
2022-07-30 22:12:16 +0200

    fix tablehighliter
    On Jenkins 2.335+ the table highlighter didn't work anymore due to
    changed layouting for inputs.
    Also when using a dark theme, the text was unreadable when hihglighted.

--- src/main/webapp/js/table.js
+++ src/main/webapp/js/table.js
@@ -22,43 +22,46 @@
  * THE SOFTWARE.
  */
 
-TableHighlighter = Class.create();
-TableHighlighter.prototype = {
+class TableHighlighter {
 
-    initialize: function(id, decalx, decaly) {
+    constructor(id, decalx, decaly) {
         this.table = $(id);
         this.decalx = decalx;
         this.decaly = decaly;
         var trs = $$('#'+this.table.id+' tr');
-        for (p=this.decaly;p<trs.length;++p){
+        for (var p=this.decaly;p<trs.length;++p){
             this.scan(trs[p]);
         }
-    },
+    };
 
-    scan: function(tr) {
+    scan(tr) {
         var element = $(tr);
         var descendants = element.getElementsByTagName('input');
-        for(q=0;q<descendants.length;++q) {
+        for (var q=0;q<descendants.length;++q) {
             var td = $(descendants[q]);
-            td.observe('mouseover', this.highlight.bind(this));
-            td.observe('mouseout', this.highlight.bind(this));
+            // before 2.335 -- TODO remove once baseline is new enough
+            td.addEventListener('mouseover', this.highlight);
+            td.addEventListener('mouseout', this.highlight);
+            // For Jenkins 2.335+
+            td.nextSibling.addEventListener('mouseover', this.highlight);
+            td.nextSibling.addEventListener('mouseout', this.highlight);
         }
-    },
+    };
 
-    highlight: function(e) {
-        var td = Event.element(e).parentNode;
+    highlight = e => {
+        var td = findAncestor(Event.element(e), "TD")
         var tr = td.parentNode;
         var trs = $$('#'+this.table.id+' tr');
         var position = td.previousSiblings().length;
 
-        for (p=this.decaly-1;p<trs.length;++p){
+        for (var p=this.decaly-1;p<trs.length;++p){
             var element = $(trs[p]);
             var num = position;
-            if(p==1) num = num - this.decalx;
+            if (p==1) num = num - this.decalx;
             element.immediateDescendants()[num ].toggleClassName('highlighted');
         }
         tr.toggleClassName('highlighted');
-    }
+    };
 
 };
 
