123cb11832d7a63a07da3602d0d05a6a2210402c
Markus Winter <m.winter@sap.com>
2022-08-14 20:04:56 +0200

    [JENKINS-69102] disable implied permissions
    When a permission is granted that implies other permissions the
    checkboxes of these permissions get disabled and the tooltip is updated
    accordingly

--- src/main/webapp/js/tableManage.js
+++ src/main/webapp/js/tableManage.js
@@ -40,6 +40,16 @@ function filterRows(filter, table) {
 }
 
 
+getPattern = function(row) {
+  let pattern = "";
+  patternEditInputs = row.getElementsByClassName("patternEdit");
+  if (patternEditInputs.length > 0) {
+    pattern = patternEditInputs[0].value;
+  }
+  return pattern;
+}
+
+
 Behaviour.specify(".row-input-filter", "RoleBasedAuthorizationStrategy", 0, function(e) {
   e.onkeyup = function() {
       let filter = e.value.toUpperCase();
@@ -63,17 +73,6 @@ Behaviour.specify("img.icon-pencil", 'RoleBasedAuthorizationStrategy', 0, functi
 });
 
 
-endPatternInput = function(inputNode) {
-  let pattern = inputNode.childNodes[0].value;
-  inputNode.innerHTML = '<a href="#" class="patternAnchor">&quot;' + pattern.escapeHTML() + '&quot;</a><input type="hidden" name="[pattern]" value="' + pattern + '"/>';
-  if (findAncestor(inputNode,"TABLE").getAttribute('id') === "projectRoles") {
-    bindListenerToPattern(inputNode.children[0]);
-  } else {
-    bindAgentListenerToPattern(inputNode.children[0]);
-  }
-  updateTooltips(inputNode, pattern);
-}
-
 preventFormSubmit = function(e) {
   var key = e.charCode || e.keyCode || 0;     
   if (key == 13) {
@@ -84,16 +83,42 @@ preventFormSubmit = function(e) {
   }
 };
 
-updateTooltips = function(inputNode, pattern) {
+endPatternInput = function(inputNode) {
+  let pattern = inputNode.childNodes[0].value;
+  let table = findAncestor(inputNode,"TABLE");
+  inputNode.innerHTML = '<a href="#" class="patternAnchor">&quot;' + pattern.escapeHTML() + '&quot;</a><input class="patternEdit" type="hidden" name="[pattern]" value="' + pattern + '"/>';
+  if (table.getAttribute('id') === "projectRoles") {
+    bindListenerToPattern(inputNode.children[0]);
+  } else {
+    bindAgentListenerToPattern(inputNode.children[0]);
+  }
   let row = findAncestor(inputNode, "TR");
-  for (td of row.getElementsByTagName('TD')) {
-    let tooltipTemplate = td.getAttribute("data-tooltip-template");
-    if (tooltipTemplate != null) {
-      let tooltip = tooltipTemplate.replace("{{PATTERNTEMPLATE}}", doubleEscapeHTML(pattern));
-      td.setAttribute("tooltip", tooltip);
+  for (td of row.getElementsByClassName('permissionInput')) {
+    updateTooltip(row, td, pattern);
+  }
+  Behaviour.applySubtree(row,true);
+}
+
+updateTooltip = function(tr, td, pattern) {
+  let tooltipTemplate = td.getAttribute("data-tooltip-template");
+  let impliedByString = td.getAttribute('data-implied-by-list');
+  let impliedByList = impliedByString.split(" ");
+  let input = td.getElementsByTagName('INPUT')[0];
+  input.disabled = false;
+  let tooltip = tooltipTemplate.replace("{{PATTERNTEMPLATE}}", doubleEscapeHTML(pattern)).replace("{{GRANTBYOTHER}}", "");
+  td.setAttribute("tooltip", tooltip);
+
+  for (let permissionId of impliedByList) {
+    let reference = tr.querySelector("td[data-permission-id='" + permissionId + "'] input");
+    if (reference !== null) {
+      if (reference.checked) {
+        input.disabled = true;
+        tooltip = tooltipTemplate.replace("{{PATTERNTEMPLATE}}", doubleEscapeHTML(pattern)).replace("{{GRANTBYOTHER}}", " is granted through another permission");;
+        td.setAttribute('tooltip', tooltip); // before 2.335 -- TODO remove once baseline is new enough
+        td.nextSibling.setAttribute('tooltip', tooltip); // 2.335+
+      }
     }
   }
-  Behaviour.applySubtree(findAncestor(inputNode,"TABLE"),true);
 }
 
 
@@ -144,30 +169,23 @@ addButtonAction = function (e, master, table, tableHighlighter, tableId) {
     }
   }
 
-  copy = document.importNode(master,true);
+  let copy = document.importNode(master,true);
   copy.removeAttribute("id");
   copy.removeAttribute("style");
-  if (tableId === "globalRoles") {
-    copy.childNodes[1].innerHTML = escapeHTML(name);
-  } else {
-    let child = copy.childNodes[1];
+  let child = copy.childNodes[1];
+  child.textContent = escapeHTML(name);
+  if (tableId !== "globalRoles") {
     let doubleQuote = '"';
-    child.textContent = escapeHTML(name);
-    let patternString = doubleQuote + escapeHTML(pattern) + doubleQuote;
-    let spanElement = copy.childNodes[2].childNodes[0].childNodes[1];
-    spanElement.innerHTML = '<a href="#" class="patternAnchor">' + patternString + '</a>';
-
-    let hidden = document.createElement('input');
-    hidden.setAttribute('name', '[pattern]');
-    hidden.setAttribute('type', 'hidden');
-    hidden.setAttribute('value', pattern);
-    spanElement.appendChild(hidden);
+    copy.getElementsByClassName("patternAnchor")[0].textContent = doubleQuote + escapeHTML(pattern) + doubleQuote;
+    copy.getElementsByClassName("patternEdit")[0].value = pattern;
   }
 
-  let children = copy.childNodes;
-  children.forEach(function(item){
-    item.outerHTML = item.outerHTML.replace(/{{ROLE}}/g, doubleEscapeHTML(name)).replace("{{PATTERN}}", doubleEscapeHTML(pattern));
-  });
+  let children = copy.getElementsByClassName("permissionInput");
+  for (let child of children) {
+    if (child.hasAttribute('data-tooltip-template')) {
+      child.setAttribute("data-tooltip-template", child.getAttribute("data-tooltip-template").replace(/{{ROLE}}/g, doubleEscapeHTML(name)).replace("{{PATTERN}}", doubleEscapeHTML(pattern)));
+    }
+  }
 
   if (tableId !== "globalRoles") {
     spanElement = copy.childNodes[2].childNodes[0].childNodes[1];
@@ -181,7 +199,7 @@ addButtonAction = function (e, master, table, tableHighlighter, tableId) {
   copy.setAttribute("name",'['+name+']');
   tbody.appendChild(copy);
   tableHighlighter.scan(copy);
-  Behaviour.applySubtree(findAncestor(tbody,"TABLE"), true);
+  Behaviour.applySubtree(findAncestor(copy,"TABLE"), true);
 }
 
 
@@ -209,4 +227,15 @@ Behaviour.specify(".global-matrix-authorization-strategy-table A.remove", 'RoleB
     }
     return false;
   }
+});
+
+Behaviour.specify(".global-matrix-authorization-strategy-table td.permissionInput input", 'RoleBasedAuthorizationStrategy', 0, function(e) {
+  let row = findAncestor(e,"TR");
+  let pattern = getPattern(row);
+  let td = findAncestor(e,"TD");
+  updateTooltip(row, td, pattern);
+  e.onchange = function() {
+    Behaviour.applySubtree(findAncestor(row,"TABLE"),true);
+    return true;
+  };
 });
