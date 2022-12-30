jenkinsci/role-strategy-plugin

commit 9846c7351a41b7e2438f755812dbd51a63cda88a

fix spotbugs

src/main/java/org/jenkinsci/plugins/rolestrategy/permissions/PermissionHelper.java
@@ -100,7 +100,7 @@ public class PermissionHelper {
    * @param p Permission
    * @return {@code true} if the permission is considered as dangerous.
    */
-  public static boolean isDangerous(@NonNull Permission p) {
+  public static boolean isDangerous(Permission p) {
     return DANGEROUS_PERMISSIONS.contains(p);
   }
 
@@ -143,7 +143,7 @@ public class PermissionHelper {
 
   private static @CheckForNull Permission getSafePermission(String id) {
     Permission permission = Permission.fromId(id);
-    if (isDangerous(permission)) {
+    if (permission != null && isDangerous(permission)) {
       LOGGER.log(Level.WARNING, "The permission: '"  + permission + "' is dangerous and will be ignored.");
       return null;
     }

commit 55c07d8e3dc50ae7ae2f8ec2aee17e15d4e6b6dc

[JENKINS-62124] NPE when exporting role without permissions with casc
Fix regression introduced with PR #236
Add unit test

src/main/java/org/jenkinsci/plugins/rolestrategy/permissions/PermissionHelper.java
@@ -124,6 +124,9 @@ public class PermissionHelper {
    */
   @CheckForNull
   public static String findPermissionId(String id) {
+    if (id == null) {
+      return null;
+    }
     List<PermissionGroup> pgs = PermissionGroup.getAll();
     Matcher m = PERMISSION_PATTERN.matcher(id);
     if (m.matches()) {

src/test/java/org/jenkinsci/plugins/rolestrategy/ConfigurationAsCodeTest.java
@@ -146,4 +146,18 @@ public class ConfigurationAsCodeTest {
     assertThat(rbas.getRoleMap(RoleType.Global).getRole("dangerous").hasPermission(PluginManager.UPLOAD_PLUGINS), is(false));
     assertThat(rbas.getRoleMap(RoleType.Global).getRole("dangerous").hasPermission(Jenkins.RUN_SCRIPTS), is(false));
   }
+
+  @Test
+  @ConfiguredWithCode("Configuration-as-Code-no-permissions.yml")
+  public void exportWithEmptyRole() throws Exception {
+    ConfiguratorRegistry registry = ConfiguratorRegistry.get();
+    ConfigurationContext context = new ConfigurationContext(registry);
+    CNode yourAttribute = getJenkinsRoot(context).get("authorizationStrategy");
+
+    String exported = toYamlString(yourAttribute);
+    String expected = toStringFromYamlFile(this, "Configuration-as-Code-no-permissions-export.yml");
+
+    assertThat(exported, is(expected));
+
+  }
 }

src/test/resources/org/jenkinsci/plugins/rolestrategy/Configuration-as-Code-no-permissions-export.yml
@@ -1,0 +1,83 @@
+roleBased:
+  roles:
+    agents:
+    - name: "agent-builder"
+      pattern: "build-agent"
+      permissions:
+      - "Agent/Build"
+    global:
+    - assignments:
+      - "builders"
+      - "builder"
+      name: "access"
+      pattern: ".*"
+      permissions:
+      - "Overall/Read"
+      - "Agent/Build"
+    - assignments:
+      - "administrators"
+      name: "admin"
+      pattern: ".*"
+      permissions:
+      - "Overall/Administer"
+    - assignments:
+      - "global-build-user"
+      name: "builder"
+      pattern: ".*"
+      permissions:
+      - "Job/Cancel"
+      - "Overall/Read"
+      - "Job/Build"
+      - "Job/Read"
+      - "Job/Workspace"
+    - assignments:
+      - "global-creator-user"
+      name: "creator"
+      pattern: ".*"
+      permissions:
+      - "Overall/Read"
+      - "Job/Delete"
+      - "Job/Create"
+      - "Job/Read"
+    items:
+    - assignments:
+      - "item-creator-user"
+      name: "@CurrentUserIsOwner"
+      pattern: ".*"
+      permissions:
+      - "Job/Cancel"
+      - "Job/Build"
+      - "Job/Delete"
+      - "Job/Read"
+      - "Job/Configure"
+      - "Job/Workspace"
+    - assignments:
+      - "item-builder-user"
+      - "builders"
+      name: "folder-access"
+      pattern: "(?i)folder"
+      permissions:
+      - "Job/Read"
+    - assignments:
+      - "item-builder-user"
+      - "builders"
+      name: "folder-builder"
+      pattern: "(?i)folder/.*"
+      permissions:
+      - "Job/Cancel"
+      - "Job/Build"
+      - "Job/Read"
+      - "Job/Workspace"
+    - assignments:
+      - "item-creator-user"
+      name: "folder-creator"
+      pattern: "(?i)folder/.*"
+      permissions:
+      - "Job/Cancel"
+      - "Job/Build"
+      - "Job/Create"
+      - "Job/Read"
+      - "Job/Configure"
+      - "Job/Workspace"
+    - name: "tester"
+      pattern: ".*"

src/test/resources/org/jenkinsci/plugins/rolestrategy/Configuration-as-Code-no-permissions.yml
@@ -1,0 +1,85 @@
+jenkins:
+  authorizationStrategy:
+    roleBased:
+      roles:
+        agents:
+          - name: "agent-builder"
+            pattern: "build-agent"
+            permissions:
+              - "Agent/Build"
+        global:
+          - assignments:
+              - "builders"
+              - "builder"
+            name: "access"
+            pattern: ".*"
+            permissions:
+              - "Overall/Read"
+              - "Agent/Build"
+          - assignments:
+              - "administrators"
+            name: "admin"
+            pattern: ".*"
+            permissions:
+              - "Overall/Administer"
+          - assignments:
+              - "global-build-user"
+            name: "builder"
+            pattern: ".*"
+            permissions:
+              - "Job/Cancel"
+              - "Overall/Read"
+              - "Job/Build"
+              - "Job/Read"
+              - "Job/Workspace"
+          - assignments:
+              - "global-creator-user"
+            name: "creator"
+            pattern: ".*"
+            permissions:
+              - "Overall/Read"
+              - "Job/Delete"
+              - "Job/Create"
+              - "Job/Read"
+        items:
+          - assignments:
+              - "item-creator-user"
+            name: "@CurrentUserIsOwner"
+            pattern: ".*"
+            permissions:
+              - "Job/Cancel"
+              - "Job/Build"
+              - "Job/Delete"
+              - "Job/Read"
+              - "Job/Configure"
+              - "Job/Workspace"
+          - assignments:
+              - "item-builder-user"
+              - "builders"
+            name: "folder-access"
+            pattern: "(?i)folder"
+            permissions:
+              - "Job/Read"
+          - assignments:
+              - "item-builder-user"
+              - "builders"
+            name: "folder-builder"
+            pattern: "(?i)folder/.*"
+            permissions:
+              - "Job/Cancel"
+              - "Job/Build"
+              - "Job/Read"
+              - "Job/Workspace"
+          - assignments:
+              - "item-creator-user"
+            name: "folder-creator"
+            pattern: "(?i)folder/.*"
+            permissions:
+              - "Job/Cancel"
+              - "Job/Build"
+              - "Job/Create"
+              - "Job/Read"
+              - "Job/Configure"
+              - "Job/Workspace"
+          - name: "tester"
+            pattern: ".*"